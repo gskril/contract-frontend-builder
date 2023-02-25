5ff0534c017a43fcf8135b3c5b7aa9c6cc57238a
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-02-25 15:23:54 -0500

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Web3 Starter</title>
+        <meta name="description" content="" />
+
+        <meta property="og:image" content="" />
+        <meta property="og:title" content="" />
+        <meta property="og:description" content="" />
+      </Head>
+
+      <main></main>
+    </>
+  )
+}
